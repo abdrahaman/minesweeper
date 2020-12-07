ca09e68996022987a4d7872113f0f3e9fde756f9
Rahaman <abrahaman@deloitte.com>
2020-12-07 23:18:01 +0530

    minesweeper 1.0

---
+++ src/component/Cell.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Cell extends React.Component {
+  getValue() {
+    const { value } = this.props;
+
+    if (!value.isClicked) {
+      return this.props.value.isFlagged ? "🏁" : null;
+    }
+    if (value.isMine) {
+      return "💥";
+    }
+    if (value.neighbour === 0) {
+      return null;
+    }
+    return value.neighbour;
+  }
+
+  render() {
+    const { value, onClick, cMenu } = this.props;
+    let className =
+      "cell" +
+      (value.isClicked ? "" : " hidden") +
+      (value.isMine ? " is-mine" : "") +
+      (value.isFlagged ? " is-flag" : "");
+
+    return (
+      <div
+        onClick={onClick}
+        className={className}
+        onContextMenu={cMenu}
+      >
+        {this.getValue()}
+      </div>
+    );
+  }
+}
+
